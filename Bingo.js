49b52c8e34a281a0f07ca3e35a5e7743da9d1f3b
Bilal Ahmad <bilal.ahmad0796@gmail.com>
2021-09-30 20:54:28 +0200

    Highlighted cells with random numbers.
    each cell will highlight in corresponding with the random number.
    
    Also a new 24 random numbers board is created that highlight with same random number generated.

--- Bingo.js
+++ Bingo.js
@@ -1,11 +1,7 @@
-let cellsContainer={
-    arrayofCells:[]
-}
 
 const displayCells = function() {
     
     //TODO: getting all the cells from (1 to 76)
-    
     const boardcellscontainerNode=document.getElementById("BoardCells-container")
     
     for(cellnumber=1; cellnumber<=76; cellnumber++){
@@ -20,13 +16,32 @@ const displayCells = function() {
     }
 }
 
-const randomNumbergenerator = function(){
-    let randomNumberNode = Math.floor(Math.random()*76)
-    alert(randomNumberNode)
-    // if(randomNumberNode>=1&&randomNumberNode<=76){
-    // }
-}
 
+const random24Numbergenerator = function(){
+    let randomNumberNode24 = Math.floor(Math.random()*24)+1
+    return randomNumberNode24
+}
+const randomNumbergenerator = function(){
+    let randomNumberNode = Math.floor(Math.random()*76)+1
+    let cellsarray= document.querySelectorAll(".cells")
+    let cells24array= document.querySelectorAll(".cells24")
+    cellsarray[randomNumberNode-1].classList.add("highlight")
+    cells24array[randomNumberNode-1].classList.add("highlight24")
+    
+}
+const displaycells24 = function(){
+    //TODO: getting 24 random numbers keyboard
+    const displaycells24Container = document.getElementById("24boardcells-random")
+    for (cellnumbers24 = 1; cellnumbers24 <= 24; cellnumbers24++){
+        let cellsforboard24 = document.createElement("div")
+        cellsforboard24.classList.add("cells24")
+        //let randomnumber24 = Math.floor(Math.random()*cellsforboard24)+1
+        //alert(randomnumber24)
+        cellsforboard24.innerText = random24Numbergenerator() 
+        displaycells24Container.appendChild(cellsforboard24)
+    }
+}
 window.onload = function() {
     displayCells()
+    displaycells24()
 }
